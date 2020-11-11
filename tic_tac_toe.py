92f3f170ea4688f18ce627bbe60cd4cbb3222028
KriszTuri <turi.krisztina1999@gmail.com>
2020-11-11 09:10:01 +0100

    added functions, not working yet

--- tic_tac_toe.py
+++ tic_tac_toe.py
@@ -1,7 +1,8 @@
+import os
+
 def init_board():
     """Returns an empty 3-by-3 board (with .)."""
-    board = []
-    #kiscica
+    board = [['.','.','.'],['.','.','.'],['.','.','.']]
     return board
 
 
@@ -29,12 +30,33 @@ def has_won(board, player):
 
 def is_full(board):
     """Returns True if board is full."""
-    return False
+    board = init_board()
+    how_many_are_empty = 0
+    for i, x in enumerate(board):
+        for j, a in enumerate(x):
+            if "." in a:
+                how_many_are_empty+=1
+    if how_many_are_empty == 0:
+        full = True
+        print("Board is full")
+    if full == True:
+        print("Game over.")
+        exit(0)
+    
 
 
 def print_board(board):
     """Prints a 3-by-3 board on the screen with borders."""
-    pass
+    rows=["A","B","C"]
+    columns =["  ","1  ","2  ","3"]
+    board = init_board()
+    i = 0
+    n = max(len(x) for l in board for x in l)
+    print(*columns)
+    for row in board:
+        print(rows[i],'|'.join(x.center(n + 2) for x in row))
+        print(" ","---+---+---")
+        i+=1
 
 
 def print_result(winner):
@@ -55,7 +77,30 @@ def tictactoe_game(mode='HUMAN-HUMAN'):
 
 
 def main_menu():
-    tictactoe_game('HUMAN-HUMAN')
+    width = os.get_terminal_size().columns
+    clear = lambda: os.system('clear')
+    print("This is the main menu.".center(width))
+    print("Press 1 if you want to play against a stupid AI.".center(width))
+    print("Press 2 if you want to play against another player.".center(width))
+    players_choice=input("")
+    while players_choice != "1" or "2": 
+
+        if players_choice == "1":
+            game_mode = "AI"
+            print("Playing against an AI. Good luck. ;)")
+            return(game_mode)
+        if players_choice == "2":
+            game_mode = "HUMAN-HUMAN"
+            print("Playing against another player. Good luck. ;)")
+            return(game_mode)
+        else:
+            clear()
+            print("You trying to be sneaky, right? Well, you can't choose anything else.".center(width))
+            print("")
+            print("This is the main menu.".center(width))
+            print("Press 1 if you want to play against a stupid AI.".center(width))
+            print("Press 2 if you want to play against another player.".center(width))
+            players_choice=input("")
 
 
 if __name__ == '__main__':
